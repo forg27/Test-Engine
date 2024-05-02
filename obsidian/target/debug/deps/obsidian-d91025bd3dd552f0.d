3bf18ab83337d6d5d10d85bedb47572e03dcd268
forg27 <cooperdietz9@gmail.com>
2024-05-01 21:07:21 -0400

    yes

---
+++ obsidian/target/debug/deps/obsidian-d91025bd3dd552f0.d
@@ -0,0 +1,5 @@
+C:\Users\inven\Test-Engine\obsidian\target\debug\deps\libobsidian-d91025bd3dd552f0.rmeta: src\lib.rs
+
+C:\Users\inven\Test-Engine\obsidian\target\debug\deps\obsidian-d91025bd3dd552f0.d: src\lib.rs
+
+src\lib.rs:
