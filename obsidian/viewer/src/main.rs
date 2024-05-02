9d069c61c58e15da79c157eacc25e9b7ab06d0f7
forg27 <cooperdietz9@gmail.com>
2024-05-01 21:18:33 -0400

    sea

--- obsidian/viewer/src/main.rs
+++ obsidian/viewer/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+// obsidian/viewer/src/main.rs
+use anyhow::Result;
+use log::info;
+use obsidian::app::{run_application, AppConfig, Application, Run};
+
+pub struct Viewer;
+
+impl Run for Viewer {
+    fn initialize(&mut self, _application: &mut Application) -> Result<()> {
+        info!("Viewer initialized");
+        Ok(())
+    }
+
+    fn update(&mut self, _application: &mut Application) -> Result<()> {
+        Ok(())
+    }
+}
+
+fn main() -> Result<()> {
+    let viewer = Viewer {};
+    run_application(
+        viewer,
+        AppConfig {
+            title: "Obsidian Viewer".to_string(),
+            logfile_name: "viewer.log".to_string(),
+            ..Default::default()
+        },
+    )
 }
