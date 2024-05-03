d2345aac4afc05060271ab10ed81d41248748d66
forg27 <cooperdietz9@gmail.com>
2024-05-03 16:38:18 -0400

    Ok

--- obsidian/crates/obsidian_render/src/vulkan/render.rs
+++ obsidian/crates/obsidian_render/src/vulkan/render.rs
@@ -1,3 +1,14 @@
+use crate::Render;
+use anyhow::Result;
+use raw_window_handle::HasRawWindowHandle;
+use log::info;
 pub(crate) use self::render::VulkanRenderBackend;
 
-mod render;
+pub(crate) struct VulkanRenderBackend;
+
+impl Render for VulkanRenderBackend{
+    fn render(&mut self, _dimensions: &[u32; 2],
+    ) ->Result<()> {
+        Ok(())
+    }
+}
